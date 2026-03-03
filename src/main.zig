c7f02aa4313315a7963c2a1ef505933cdab68800
QuiteClose <dean@quiteclose.net>
2026-03-02 18:34:14 -0800

    All djot tests pass.

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,63 @@ const std = @import("std");
 const zjot = @import("zjot");
 
 pub fn main() !void {
-    const stderr = std.io.getStdErr().writer();
-    try stderr.print("zjot: not yet implemented\n", .{});
+    var gpa: std.heap.GeneralPurposeAllocator(.{}) = .init;
+    defer _ = gpa.deinit();
+    const a = gpa.allocator();
+
+    const args = try std.process.argsAlloc(a);
+    defer std.process.argsFree(a, args);
+
+    var mode: enum { html, ast } = .html;
+    var sourcepos = false;
+    var file_path: ?[]const u8 = null;
+
+    for (args[1..]) |arg| {
+        if (std.mem.eql(u8, arg, "--ast")) {
+            mode = .ast;
+        } else if (std.mem.eql(u8, arg, "--sourcepos")) {
+            sourcepos = true;
+        } else if (std.mem.eql(u8, arg, "--help") or std.mem.eql(u8, arg, "-h")) {
+            try printUsage();
+            return;
+        } else if (arg.len > 0 and arg[0] == '-') {
+            const stderr = std.fs.File.stderr().writer();
+            try stderr.print("zjot: unknown option: {s}\n", .{arg});
+            std.process.exit(1);
+        } else {
+            file_path = arg;
+        }
+    }
+
+    const input = if (file_path) |path|
+        try std.fs.cwd().readFileAlloc(a, path, 10 * 1024 * 1024)
+    else
+        try std.fs.File.stdin().readToEndAlloc(a, 10 * 1024 * 1024);
+    defer a.free(input);
+
+    const output = switch (mode) {
+        .html => try zjot.toHtml(a, input),
+        .ast => try zjot.toAstOpts(a, input, sourcepos),
+    };
+    defer a.free(output);
+
+    const stdout = std.fs.File.stdout().writer();
+    try stdout.writeAll(output);
+}
+
+fn printUsage() !void {
+    const stdout = std.fs.File.stdout().writer();
+    try stdout.writeAll(
+        \\Usage: zjot [OPTIONS] [FILE]
+        \\
+        \\Parse Djot markup and produce output.
+        \\
+        \\If FILE is omitted, reads from stdin.
+        \\
+        \\Options:
+        \\  --ast         Output AST instead of HTML
+        \\  --sourcepos   Include source positions in AST output
+        \\  -h, --help    Show this help message
+        \\
+    );
 }
--- src/root.zig
+++ src/root.zig
@@ -1,19 +1,31 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const Parser = @import("Parser.zig");
+const html = @import("html.zig");
+const ast = @import("ast.zig");
+const Node = @import("node.zig").Node;
 
-pub fn toHtml(a: Allocator, input: []const u8) Allocator.Error![]const u8 {
-    _ = input;
-    return a.dupe(u8, "");
+pub fn toHtml(a: Allocator, input: []const u8) ![]const u8 {
+    var shared = Parser.SharedState{};
+    var parser = Parser.init(a, input, &shared);
+    const doc = try parser.parseDoc();
+    var out: std.ArrayList(u8) = .{};
+    try html.renderNode(a, &out, doc);
+    return out.toOwnedSlice(a);
 }
 
-pub fn toAst(a: Allocator, input: []const u8) Allocator.Error![]const u8 {
+pub fn toAst(a: Allocator, input: []const u8) ![]const u8 {
     return toAstOpts(a, input, false);
 }
 
-pub fn toAstOpts(a: Allocator, input: []const u8, sourcepos: bool) Allocator.Error![]const u8 {
-    _ = input;
-    _ = sourcepos;
-    return a.dupe(u8, "");
+pub fn toAstOpts(a: Allocator, input: []const u8, sourcepos: bool) ![]const u8 {
+    var shared = Parser.SharedState{};
+    var parser = Parser.init(a, input, &shared);
+    parser.track_pos = sourcepos;
+    const doc = try parser.parseDoc();
+    var out: std.ArrayList(u8) = .{};
+    try ast.renderAstNode(a, &out, doc, 0, true);
+    return out.toOwnedSlice(a);
 }
 
 test {
